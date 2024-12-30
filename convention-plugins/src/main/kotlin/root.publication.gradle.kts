38aaff588260fefdfee79616cdf72903a1054ed7
kigawa <kigawa.8390@gmail.com>
2024-12-30 17:38:32 +0900

    add: cd

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin")
 }
 object Conf {
-    const val GROUP = "net.kigawa.hakate"
+    const val GROUP = "net.kigawa"
     const val VERSION = "1.0.0"
 }
 
@@ -13,28 +13,11 @@ allprojects {
     version = Conf.VERSION
 }
 
-//publishing {
-//    repositories {
-//        maven {
-//            name = "OSSRH"
-//            url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-//            credentials {
-//                username = System.getenv("MAVEN_USERNAME")
-//                password = System.getenv("MAVEN_PASSWORD")
-//            }
-//        }
-//    }
-//}
 nexusPublishing {
     // Configure maven central repository
     // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
     repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-//            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-//            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username = System.getenv("MAVEN_USERNAME")
-            password = System.getenv("MAVEN_PASSWORD")
-        }
+        sonatype()
 
     }
 }
